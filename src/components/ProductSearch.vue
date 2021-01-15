9fa0cd57daa9fe5cda93df637cbc30cc9f9b163b
Swetha Narayan <swethanarayan@Swethas-MacBook-Pro.local>
2021-01-15 15:59:36 +0530

    commit 2

---
+++ src/components/ProductSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="productSearch">
+      <h2>{{product.productId}}</h2>
+      <h3>{{product.category}}</h3>
+      <p><strong>Stock remaining: {{product.stock}}</strong></p>
+      <p>{{product.attribute}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductSearch',
+  data: () => ({
+    product: {
+      productId: 'prod1',
+      category: 'headphone',
+      stock: 5,
+      attribute: 'okay'
+      }
+  }),
+  props: ['product']
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+      <ProductSearch v-for="product in products" v-bind:key="product.productId" v-bind:category="product.category" v-bind:stock="product.stock" v-bind:attribute="product.attribute"></ProductSearch>
+  </div>
+</template>
+
+<script>
+import ProductSearch from 'src/components/ProductSearch.vue'
+export default {
+  name: 'Search',
+  components: {
+    ProductSearch: {productId:'prod1',category:'headphone',stock:5,attribute:'okay'}
+  },
+  data: () => ({
+      products
+  })
+}
+</script>
+
+<style>
+</style>
