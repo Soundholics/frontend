7df6029a7971ec7a3828df5d4744df39cd455b5b
Swetha Narayan <swethanarayan@Swethas-MacBook-Pro.local>
2021-01-15 19:31:12 +0530

    commit 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div> -->
+    <nav id="cat">
+    <figure>
+      <img src="../assets/headphone.svg" alt="Headphone">
+      <figcaption>Headphones</figcaption>
+    </figure>
+    <!-- <img src="src/assets/earphone.png" alt="Earphone">
+    <figcaption>Earphone</figcaption> -->
+    <figure>
+      <img src="../assets/earphone.svg" alt="Earphone">
+      <figcaption>Earphone</figcaption>
+    </figure>
+    <figure>
+      <img src="../assets/speaker.svg" alt="Speaker">
+      <figcaption>Speaker</figcaption>
+    </figure>
+    <figure>
+      <img src="../assets/earbud.svg" alt="Earbuds">
+      <figcaption>Earbuds</figcaption>
+    </figure>
+    <figure>
+      <img src="../assets/audioplayer.svg" alt="Audio Players">
+      <figcaption>Audio Players</figcaption>
+    </figure>
+    </nav>
+    <div class="flex-container">
+      <img src="../assets/speaker-temp.jpeg" alt="" class="temp">
+      <img src="../assets/earphone-temp.jpeg" alt="" class="temp">
+      <img src="../assets/earbuds-temp.jpeg" alt="" class="temp">
+      <img src="../assets/headphone-temp.jpeg" alt="" class="temp">
+      <img src="../assets/audioplayer-temp.jpeg" alt="" class="temp">
+    </div>
   </div>
 </template>
 
@@ -10,10 +38,40 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
-// export default {
-//   name: 'Home',
+export default {
+  name: 'Home'
 //   components: {
 //     HelloWorld
 //   }
-// }
+}
 </script>
+<style>
+figure{
+  display: inline-block;
+  margin: 6px;
+}
+figure img {
+    /* height: 75px; */
+    height: 3em;
+  }
+  #cat{
+    padding-top: 70px;
+    display: flex;
+    justify-content:space-around;
+    background-color: #323232;
+    color: #00ccff;
+  }
+  .flex-container{
+    display: flexbox;
+    flex-direction: row;
+    margin-top: 20px;
+    background-color: #0d7377;
+  }
+  .temp{
+    max-width: 20%;
+    height: 60rem;
+  }
+  #home{
+    background-color: #0d7377;
+  }
+</style>
