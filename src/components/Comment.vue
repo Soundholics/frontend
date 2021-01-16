2bb981d2e92a7fed695395c2bf371aeff76a8446
Swetha Narayan <swethanarayan@Swethas-MacBook-Pro.local>
2021-01-16 14:52:20 +0530

    commit

---
+++ src/components/Comment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="review">
+      <label for="name">{{cname}}</label>
+      <label for="rating">Rated: </label>
+      {{rating.rate}}
+      <article>{{rating.review}}</article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: ['rating'],
+  data: () => ({
+    cname: 'Guest' // take customer name through id
+  })
+}
+</script>
+
+<style>
+.review{
+  border: 2px solid #323232;
+  margin:10px;
+}
+label[for="name"]{
+  display: block;
+  text-align: end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -105,6 +105,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "register" */ '../views/Cart.vue')
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "product" */ '../views/Product.vue')
   }
 ]
 
