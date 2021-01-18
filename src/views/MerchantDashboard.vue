c36e5b3be20f9c4274b964aa50852e11ff73552a
Swetha Narayan <swethanarayan@Swethas-MacBook-Pro.local>
2021-01-18 23:05:45 +0530

    commit

--- src/views/MerchantDashboard.vue
+++ src/views/MerchantDashboard.vue
@@ -12,9 +12,28 @@
     <MerchantProduct v-for="product in products" :key="product.productName" :product="product"/>
     <div>
       <b-modal id="modal" title="Add Product">
-        <div>Product Name: <input type="text"></div>
-        <div>Quantity: <input type="number"></div>
-        <div>Images: <input type="file"></div>
+        <div><label>Product Name:</label><input type="text" v-model="product.name"></div>
+        <div><label>Quantity:</label><input type="number" v-model="inventory.quantity"></div>
+        <div><label>Features:</label><br>
+            <input type="text" v-model="inventory.price">
+                        <select v-model="brand" text="Brand">
+              <option v-for="entry in brandList" :key="entry">{{entry}}</option>
+            </select>
+                        <select v-model="ergonomics" text="Ergonomics">
+              <option v-for="entry in ergonomicsList" :key="entry">{{entry}}</option>
+            </select>
+                        <select v-model="colors" text="Colors">
+              <option v-for="entry in colorsList" :key="entry">{{entry}}</option>
+            </select>
+                        <select v-model="product.category" text="Category">
+              <option v-for="entry in categoryList" :key="entry">{{entry}}</option>
+            </select>
+                        <select v-model="rating" text="Rating">
+              <option v-for="entry in ratingList" :key="entry">{{entry}}</option>
+            </select>
+        </div>
+        <div>Images:<input type="file"></div>
+        <button @click="addProduct">Submit</button>
       </b-modal>
     </div>
   </div>
@@ -27,7 +46,36 @@ export default {
   components: { MerchantProduct },
   name: 'MerchantDashboard',
   data: () => ({
-    products: []
+    products: {
+    //   name: '',
+    //   category: '',
+    //   attributes: []
+    },
+    product: {
+      name: '',
+      category: '',
+      attribute: []
+    },
+    inventory: {
+      price: 0.0,
+      quantity: 0,
+      merchant_id: '',
+      product_id: ''
+    },
+    stock: '',
+    brand: '',
+    colors: '',
+    ergonomics: '',
+    rating: '',
+    quantity: '',
+    price: '',
+    priceList: ['Rs. 498 and Below', 'Rs. 499 - 998', 'Rs. 999 - 1498', 'Rs. 1499 - 1998', 'Rs. 1999 - 2498', 'Rs. 2499 - 4998', 'Rs. 4999 and Above'],
+    brandList: ['JBL', 'SkullCandy', 'Sony', 'Bose', 'Sennheiser', 'Ubon', 'Boat', 'Samsung', 'Apple'],
+    colorsList: ['Red', 'Black', 'Blue', 'Green', 'Cyan', 'Olive', 'Purple', 'Brown'],
+    ergonomicsList: ['Wired', 'WireLess'],
+    categoryList: ['headphones', 'earphones', 'earbuds', 'audio players', 'speakers'],
+    ratingList: ['5 stars', '4 stars', '3 stars', '2 star and above'],
+    stockList: ['exclude out of stock']
   }),
   beforeMount () {
     fetch('http://10.177.68.63:8082/Inventory/findProducts/' + 'anil@gmail.com')
@@ -35,6 +83,59 @@ export default {
       .then((res) => {
         this.products = res
       })
+  },
+  methods: {
+    addProduct () {
+      this.product.attribute = [
+        {
+          attributeKey: 'brand',
+          getAttributeValue: this.brand
+        },
+        {
+          attributeKey: 'colors',
+          getAttributeValue: this.colors
+        },
+        {
+          attributeKey: 'ergonomics',
+          getAttributeValue: this.ergonomics
+        },
+        {
+          attributeKey: 'rating',
+          getAttributeValue: this.rating
+        },
+        {
+          attributeKey: 'price',
+          getAttributeValue: this.price
+        }]
+      console.log(this.product)
+      fetch('http://10.177.68.63:8082/product/addproduct', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.product)
+      }).then(res => res.json())
+        .then(res => {
+          console.log(res)
+          this.inventory.productId = res.productId
+          this.inventory.merchantId = this.$store.getters.getmerchantEmail
+          fetch('http://10.177.68.63:8082/Inventory/save', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.inventory)
+          }).then(res => res.json())
+            .then(res => {
+              console.log(res)
+              fetch('http://10.177.68.63:8082/Inventory/findProducts/' + 'anil@gmail.com')
+                .then((res) => res.json())
+                .then((res) => {
+                  this.products = res
+                })
+            })
+        })
+    }
   }
 }
 </script>
@@ -55,5 +156,14 @@ button{
     background: white;
     border: none;
     border-bottom: 1px solid grey;
+}
+select{
+  width: 100%;
+}
+button.dropdown-toggle{
+width: 100%;
+}
+ul.dropdown-menu{
+  width: 100%;
 }
 </style>
